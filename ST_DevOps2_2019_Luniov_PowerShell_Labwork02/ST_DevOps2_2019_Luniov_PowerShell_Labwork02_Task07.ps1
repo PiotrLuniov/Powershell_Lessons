80c5e2718241388d19aed4b4e43e86f532230274
PiotrLuniov <47357332+PiotrLuniov@users.noreply.github.com>
2019-02-08 13:27:25 +0300

    Update ST_DevOps2_2019_Luniov_PowerShell_Labwork02_Task07.ps1

--- ST_DevOps2_2019_Luniov_PowerShell_Labwork02/ST_DevOps2_2019_Luniov_PowerShell_Labwork02_Task07.ps1
+++ ST_DevOps2_2019_Luniov_PowerShell_Labwork02/ST_DevOps2_2019_Luniov_PowerShell_Labwork02_Task07.ps1
@@ -1,8 +1,11 @@
 ﻿# 7. Вывести список названий и занятую виртуальную память (в Mb) каждого процесса, разделённые знаком тире, при этом если процесс занимает более 100Mb – выводить информацию красным цветом, иначе зелёным.
 
 Get-Process | ForEach-Object{ 
-if ($_.VM/1mb -gt 100) {
-Write-Host -f Red $_.ProcessName - $($_.VM/1mb)}
-else {
-Write-Host -f Green $_.ProcessName - $($_.VM/1mb)}
+    
+    if ($_.VM/1mb -gt 100) {
+        Write-Host -f Red $_.ProcessName - $($_.VM/1mb)
+    }
+    else {
+        Write-Host -f Green $_.ProcessName - $($_.VM/1mb)
+    }
 } 
