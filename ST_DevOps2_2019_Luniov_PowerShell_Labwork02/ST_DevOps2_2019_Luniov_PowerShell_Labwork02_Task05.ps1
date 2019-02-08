2d874f4a2382ce55c2433c98e20b7d0f719166b5
PiotrLuniov <47357332+PiotrLuniov@users.noreply.github.com>
2019-02-08 13:31:18 +0300

    Новая заметка

--- ST_DevOps2_2019_Luniov_PowerShell_Labwork02/ST_DevOps2_2019_Luniov_PowerShell_Labwork02_Task05.ps1
+++ ST_DevOps2_2019_Luniov_PowerShell_Labwork02/ST_DevOps2_2019_Luniov_PowerShell_Labwork02_Task05.ps1
@@ -1,4 +1,4 @@
 ﻿# 5. Просуммировать все числовые значения переменных текущего сеанса.
-
+# Новая заметка
 Get-Variable | Where-Object {$_.Value -and $_.Value.GetType().Name -eq "Int32"}| Measure-Object -Property Value -Sum 
 
